5655206d38b2af5eddef883fbc360235291139db
nibishtamrakar <nibishtamrakar@gmail.com>
2025-03-10 21:33:18 -0400

    Navbar and HomePage updated
    On navigation bar, removed links and created buttons with hover effects
    
    On homepage, added logo and login button: functionality not implemented yet

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,54 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import duckLogo from '../assets/YapYapLogo_notext.png';
+import duckLogo from '../assets/YapYapLogo_notext.png'
 
 const Navbar = () => {
     return(
         <nav className="bg-amber-400 px-10 py-2">
             <div className="w-full mx-auto flex items-center">
+                
+                {/* LOGO */}
                 <div className="flex">
                     <img src={duckLogo} alt="duck logo" className="h-12 w-12 mr-2"></img>
                 </div>
-                <div className="flex-1 text-center">
+                
+                {/* NAME */}
+                <div className="absolute left-1/2 -translate-x-1/2">
                     <h1 className="text-2xl font-bold text-amber-50">YapYap!</h1>
                 </div>
-                <div className="space-x-4">
-                    <Link to="/support" className="text-white hover:text-amber-50 font-bold">SUPPORT</Link>
-                    <Link to="/bftw" className="text-white hover:text-amber-50 font-bold">BFTW</Link>
-                    <Link to="/about" className="text-white hover:text-amber-50 font-bold">ABOUT US</Link>
+
+                {/* LINKS BUTTONS */}
+                <div className="flex gap-4 ml-auto">
+                    <button 
+                        className='
+                            bg-amber-400 text-white px-6 py-3 w-32 h-12
+                            hover:shadow-lg
+                            transition-transform transform
+                            duration-300
+                            '
+                    ><Link to="/support" className="text-white hover:text-amber-50 font-bold">SUPPORT</Link>
+                    </button>
+                    <button 
+                        className='
+                            bg-amber-400 text-white px-6 py-3 w-32 h-12
+                            hover:shadow-lg
+                            transition-transform transform
+                            duration-300
+                            '
+                    ><Link to="/bftw" className="text-white hover:text-amber-50 font-bold">BFTW</Link>
+                    </button>
+                    <button 
+                        className='
+                            bg-amber-400 text-white px-6 py-3 w-32 h-12
+                            hover:shadow-lg
+                            transition-transform transform
+                            duration-300
+                            '
+                    ><Link to="/about" className="text-white hover:text-amber-50 font-bold">ABOUT US</Link>
+                    </button>
                 </div>
+
+                {/* MORE STUFF? */}
             </div>
         </nav>
     )
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import duckLogo from '../assets/YapYapLogo_notext.png';
 
 const HomePage = () => {
   return (
     <>
     <Navbar />
         <div className="container mx-auto p-4">
-            <h2 className="text-3xl font-bold">Welcome to YapYap!</h2>
-            <p className="text-gray-700 mt-4">
-                This is the homepage of YapYap. Explore our services and learn more about us!
-            </p>
+            <div className="flex flex-col items-center justify-center h-[calc(100vh-80px)] px-8 space-y-6">
+                <img 
+                    src={duckLogo} 
+                    alt="duck logo" 
+                    className="h-60 w-60 transition-transform duration-300 hover:scale-125 relative -top-5" />
+
+                <button 
+                    className='
+                        bg-amber-400 text-white px-6 py-3
+                        rounded-lg shadow-md
+                        hover:bg-amber-500 hover:shadow-lg
+                        transition-transform transform hover:scale-105
+                        duration-300
+                        '
+                >LOGIN
+                </button>
+
+                <h1 className='font-sans'>Or Signup Here</h1>
+            </div>
         </div>
     </>
   )

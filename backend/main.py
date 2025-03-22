06839c925089c0b0a07872589271fb9a441e5853
FardinAhmed3 <ahmedfardin077@gmail.com>
2025-03-22 06:09:03 -0400

    feat: add ASGI support to integrate python-socketio

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routes.auth import router as auth_router
-from backend.mongodb.mongo_database import MongoClient
-app = FastAPI()
+from socketio import AsyncServer
+from socketio.asgi import ASGIApp
+from routes.auth import router as auth_router
+from routes.chat import register_socketio_handlers
+from routes.messages import router as messages_router
+from dotenv import load_dotenv
+import os
 
-# CORS setup
+load_dotenv()
+
+sio = AsyncServer(async_mode="asgi", cors_allowed_origins="*")
+register_socketio_handlers(sio)
+
+fastapi_app = FastAPI()
+
 origins = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://localhost:5173",
+    "*",
 ]
 
-app.add_middleware(
+fastapi_app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
@@ -19,6 +30,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Mount the auth routes
-app.include_router(auth_router)
-
+fastapi_app.include_router(auth_router)
+fastapi_app.include_router(messages_router)
+# Socket.IO ASGI
+app = ASGIApp(sio, other_asgi_app=fastapi_app)

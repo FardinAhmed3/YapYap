eed7318d5d6d0efb24aacb56833cb75994493531
nibishtamrakar <nibishtamrakar@gmail.com>
2025-03-24 21:48:25 -0400

    Update SignupPage.jsx

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import axios from "axios"
 import Navbar from "../components/Navbar"
 import SignUpForm from "../components/SignUpForm"
+import '../App.css'
 
 const SignupPage = () => {
   const [firstName, setFirstName] = useState("")
@@ -12,6 +13,7 @@ const SignupPage = () => {
   const [dob, setDob] = useState("")
   const [gender, setGender] = useState("")
 
+<<<<<<< Updated upstream
   const handleSubmit = async (e) => {
     e.preventDefault()
 
@@ -60,6 +62,54 @@ const SignupPage = () => {
       </div>
     </>
   )
+=======
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert(`Signed up with: ${firstName}, ${lastName}, ${username}, ${email}, ${password}, ${dob}, ${gender}`)
+    }
+
+    return(
+        <div>
+            <Navbar />
+
+            <div className="container mx-auto my-auto">
+                <div className="flex flex-col items-center justify-center h-screen">
+                    <img 
+                        src="/YapYapLogo.svg" 
+                        alt="Duck Logo" 
+                        className="h-60 w-60 float-animation"
+                        // style={{ animation: 'floatUpDown 2s ease-in-out infinite' }}
+                    />
+                    <img 
+                        src="/YapYapLogo.svg" 
+                        alt="Duck Logo" 
+                        className="h-15 w-15 -mb-4 -translate-x-20 -translate-y-15 float-animation-delayed"
+                        // style={{ animation: 'floatUpDown 2s ease-in-out infinite'}}
+                    />
+
+                    <SignUpForm
+                        onSubmit={handleSubmit}
+                        firstName={firstName}
+                        setFirstName={setFirstName}
+                        lastName={lastName}
+                        setLastName={setLastName}
+                        username={username}
+                        setUsername={setUsername}
+                        email={email}
+                        setEmail={setEmail}
+                        password={password}
+                        setPassword={setPassword}
+                        dob={dob}
+                        setDob={setDob}
+                        gender={gender}
+                        setGender={setGender}
+                    />
+                </div>
+
+            </div>
+        </div>
+    )
+>>>>>>> Stashed changes
 }
 
 export default SignupPage

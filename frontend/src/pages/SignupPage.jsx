f8fa59a77e6e9e9b66d99d39fc9ca9245a52faea
nibishtamrakar <nibishtamrakar@gmail.com>
2025-03-24 21:52:33 -0400

    Update SignupPage.jsx

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -13,56 +13,6 @@ const SignupPage = () => {
   const [dob, setDob] = useState("")
   const [gender, setGender] = useState("")
 
-<<<<<<< Updated upstream
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    const userData = {
-      first_name: firstName,
-      last_name: lastName,
-      username,
-      email,
-      password,
-      date_of_birth: dob,
-      gender,
-    }
-
-    try {
-      const { data } = await axios.post("http://127.0.0.1:8000/users/", null, { params: userData })
-      alert(`Signed up successfully! User ID: ${data.username}`)
-    } catch (error) {
-      console.error("Error:", error)
-      alert("Signup failed: " + (error.message))
-    }
-  }
-
-  return (
-    <>
-      <Navbar />
-      <div className="container mx-auto p-4">
-        <div className="flex flex-col items-center justify-center h-screen">
-          <SignUpForm
-            onSubmit={handleSubmit}
-            firstName={firstName}
-            setFirstName={setFirstName}
-            lastName={lastName}
-            setLastName={setLastName}
-            username={username}
-            setUsername={setUsername}
-            email={email}
-            setEmail={setEmail}
-            password={password}
-            setPassword={setPassword}
-            dob={dob}
-            setDob={setDob}
-            gender={gender}
-            setGender={setGender}
-          />
-        </div>
-      </div>
-    </>
-  )
-=======
     const handleSubmit = (e) => {
         e.preventDefault()
         alert(`Signed up with: ${firstName}, ${lastName}, ${username}, ${email}, ${password}, ${dob}, ${gender}`)
@@ -109,7 +59,6 @@ const SignupPage = () => {
             </div>
         </div>
     )
->>>>>>> Stashed changes
 }
 
 export default SignupPage
